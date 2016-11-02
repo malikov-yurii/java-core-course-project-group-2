6d77ff80cd4df5f652faaa9a8606f3bd8d759cf9
Malikov <malikov.yuriy.alex@gmail.com>
2016-11-02 16:01:29 +0200

    base skeleton v0.4 DataSource renamed (because there are too much srandard classes with name DataSource) and moved

--- src/HotelSystemDataSource.java
+++ src/HotelSystemDataSource.java
@@ -1,10 +1,8 @@
-package Model;
-
 import Model.HotelDAOImpl;
 import Model.RoomDAOImpl;
 import Model.UserDAOImpl;
 
-public class DataSourcee {
+public class HotelSystemDataSource {
     private static final UserDAOImpl userDAO = new UserDAOImpl();
     private static final RoomDAOImpl roomsDAO = new RoomDAOImpl();
     private static final HotelDAOImpl hotelDAO = new HotelDAOImpl();
