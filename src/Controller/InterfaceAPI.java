74f046a043def2a0691366cb1b1d19038eff596e
Malikov <malikov.yuriy.alex@gmail.com>
2016-11-02 14:20:21 +0200

    base skeleton v0.2 Я не пойму про Datasorce и Dao. Нужно делать так как в этом коммите? может просто сделать коллекции для Юзер

--- src/Controller/InterfaceAPI.java
+++ src/Controller/InterfaceAPI.java
@@ -6,9 +6,14 @@ import java.util.Collection;
 import java.util.Map;
 
 public interface InterfaceAPI {
+
     Collection<Hotel> findHotelbyName(String name);
+
     Collection<Hotel> findHotelbyCity(String city);
+
     void bookRoom(long roomId, long userId, long hotelId);
+
     void cancelReservation(long roomId, long userId, long hotelId);
+
     Collection<Hotel> findRoom(Map<String, String> params);
 }
---
+++ src/DataSource.java
@@ -0,0 +1,14 @@
+import Model.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class DataSource {
+    Collection<UserDAOImpl> users = new ArrayList<>();
+    Collection<RoomDAOImpl> rooms = new ArrayList<>();
+    Collection<HotelDAOImpl> hotels = new ArrayList<>();
+
+
+
+
+}
