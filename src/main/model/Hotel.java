03f54169ffd7efede7213c65f453f325ff16b603
Malikov <malikov.yuriy.alex@gmail.com>
2016-11-03 17:39:16 +0200

    hotelName was removed because Hotel extends NamedEntity

--- src/main/model/Hotel.java
+++ src/main/model/Hotel.java
@@ -2,21 +2,11 @@ package main.model;
 
 public class Hotel extends NamedEntity {
 
-    private String hotelName;
-
     private String city;
 
     public Hotel(String hotelName, String city) {
         super(hotelName);
         this.city = city;
-    }
-
-    public String getHotelName() {
-        return hotelName;
-    }
-
-    public void setHotelName(String hotelName) {
-        this.hotelName = hotelName;
     }
 
     public String getCity() {
