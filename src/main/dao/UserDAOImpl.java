68c14c43b573bd56c95dfa0dc05b24e3ae058ed6
Mykhailo <mhs.senchuk@gmail.com>
2016-11-04 14:41:35 +0200

    adder saveUser method

--- src/main/dao/UserDAOImpl.java
+++ src/main/dao/UserDAOImpl.java
@@ -8,7 +8,9 @@ public class UserDAOImpl extends AbstractDAOImpl<User> {
 
     private static UserDAOImpl singletonInstance;
 
-    private UserDAOImpl(){}
+    private UserDAOImpl(){
+
+    }
 
     public static void initialize(Collection<User> collection){
         if (singletonInstance == null) {
@@ -23,5 +25,13 @@ public class UserDAOImpl extends AbstractDAOImpl<User> {
 
     public static Collection<User> getUsers() {
         return singletonInstance.getCollection();
+    }
+
+    public static void saveUser(User user) {
+        if (singletonInstance == null) {
+            singletonInstance = new UserDAOImpl();
+        }
+        singletonInstance.save(user);
+        System.out.println("user is saved");
     }
 }
