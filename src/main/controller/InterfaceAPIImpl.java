ebde31274fe018226941ac3c6fe1d1201389ecd1
Mykhailo <mhs.senchuk@gmail.com>
2016-11-07 01:50:39 +0200

    InterfaceAPIImpl java8, DAO methods, data only from DAO, more DRY

--- src/main/controller/InterfaceAPIImpl.java
+++ src/main/controller/InterfaceAPIImpl.java
@@ -9,145 +9,150 @@ import main.model.Hotel;
 import main.model.Room;
 import main.model.User;
 
-import java.util.ArrayList;
+
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+
 /**
  * Created by Mykhailo Senchuk on 11/2/2016.
- * <p>
- * require collection of <Hotel>
- * require collection of <User>
- * require collection of <Room>
- * takes from DAO(empty constructor) or as parameters
- * <p>
- * working(additional testing is appreciated):
- * public Collection<Hotel> findHotelbyCity(String city)
+ *
+ *
+ * takes data from DAO(empty constructor)
+ *
+ * implements methods:
+ *
+ * public Collection<Hotel>  findHotelbyName(String name)
  * public Collection<Hotel> findHotelbyCity(String city)
  * public void bookRoom(long roomId, long userId, long hotelId)
  * public void cancelReservation(long roomId, long userId, long hotelId)
- * <p>
- * TODO Collection<Hotel> findRoom(Map<String, String> params)
- * <p>
- * TODO Java8
- * TODO Polymorphism
- * TODO exceptions
- * TODO separate search method
+ * public Collection<room> findRoom(Map<String, String> params)
+ *
+ *  supporting methods:
+ *  public Room searchRoom(long roomId, long hotelId)
+ *  public User searchUser(long userId)
+ *  public boolean validateRoom(Room room, User user)
+ *
+ *
+ *
+ *
+ *
+ *
  */
 public class InterfaceAPIImpl implements InterfaceAPI {
 
     private RoomDAOImpl roomDAOImpl = RoomDAOImpl.getInstance();
+    private HotelDAOImpl hotelDAOImpl = HotelDAOImpl.getInstance();
 
-    private Collection<Hotel> hotels;
-    private Collection<User> users;
-    private Collection<Room> rooms;
-
-
-    //get data from database
-    //TODO try catch for empty DB
-    public InterfaceAPIImpl() {
-        try {
-            hotels = HotelDAOImpl.getHotels();
-            users = UserDAOImpl.getUsers();
-            rooms = RoomDAOImpl.getRooms();
-        } catch (NullPointerException e) {
-            //Пока что просто обработали и отправили письмо
-            //System.out.println("There is NullPointerException");
-        }
-
-    }
-
-    //get data from input parameters
-    public InterfaceAPIImpl(List<Hotel> hotels, List<User> users, List<Room> rooms) {
-        this.hotels = hotels;
-        this.users = users;
-        this.rooms = rooms;
-    }
 
     //receive hotel name, return collection of hotels with that name
     @Override
     public Collection<Hotel> findHotelbyName(String name) {
 
-        Collection<Hotel> searchResult = new ArrayList<>();
-        for (Hotel hotel : hotels) {
-            if (hotel.getName().equals(name))
-                searchResult.add(hotel);
-        }
-        return searchResult;
+        return hotelDAOImpl.getHotelByName(name);
+
     }
 
     //receive city name, return collection of hotels in the city
     @Override
     public Collection<Hotel> findHotelbyCity(String city) {
 
-        Collection<Hotel> searchResult = new ArrayList<>();
+        return hotelDAOImpl.getHotelByCity(city);
+    }
 
-        for (Hotel hotel : hotels) {
-            if (hotel.getCity().equals(city))
-                searchResult.add(hotel);
+    public Room searchRoom(long roomId, long hotelId) {
+        try {
+            return RoomDAOImpl.getRooms()
+                    .stream()
+                    .filter(room -> room.getId() == roomId && room.getHotelId() == hotelId)
+                    .findFirst()
+                    .get();
         }
-        return searchResult;
+        catch (NoSuchElementException e){
+            System.out.println("room wasn't found");
+            return null;
+        }
+    }
+    public User searchUser(long userId) {
+        try {
+            return UserDAOImpl.getUsers()
+                    .stream()
+                    .filter(user -> user.getId() == userId)
+                    .findFirst()
+                    .get();
+        }
+        catch (NoSuchElementException e){
+            System.out.println("user wasn't found");
+            return null;
+        }
+    }
+
+    public boolean validateRoom(Room room, User user){
+        //break method if room is reserved
+        if (room == null) {
+            return false;
+        }
+
+        //break method if user wasn't found
+        if (user == null) {
+            return false;
+        }
+        return true;
     }
 
     //searches for Room with given parameters and set isReserved in Room for true
     @Override
     public void bookRoom(long roomId, long userId, long hotelId) {
 
-        //search room
-        Room roomSearch = null;
-        for (Room room : rooms) {
-            if (room.getId() == roomId && room.getHotelId() == hotelId) {
-                roomSearch = room;
-                break;
-            }
-        }
+        //search room and user
+        Room room = null;
+        room = searchRoom(roomId,hotelId);
+        User user = null;
+        user = searchUser(userId);
 
-        //break method if room wasn't found
-        if (roomSearch == null) {
-            System.out.println("room wasn't found");
-            return;
-        }
+        //break method if room or user wasn't found
+        if(!validateRoom(room,user)) return;
 
         //break method if room is already reserved
-        if (roomSearch.isReserved()) {
+        if (room.isReserved()) {
             System.out.println("room is already reserved");
             return;
         }
 
-        //search user
-        User userSearch = null;
-        for (User user : users) {
-            if (user.getId() == userId) {
-                userSearch = user;
-                break;
-            }
-        }
-
-        //break method if user wasn't found
-        if (userSearch == null) {
-            System.out.println("user wasn't found");
-            return;
-        }
-
-        //break method if room is already reserved
-        if (roomSearch.isReserved()) {
-            System.out.println("room is already reserved");
-            return;
-        }
-
-        //add user
-        roomSearch.setUserReserved(userSearch);
+        //add user to the room
+        room.setUserReserved(user);
 
         //set isReserved flag for true
-        roomSearch.setReserved(true);
+        room.setReserved(true);
         System.out.println("room is reserved");
+    }
+    //searches for Room with given parameters and set isReserved in Room for false
+    @Override
+    public void cancelReservation(long roomId, long userId, long hotelId) {
+        //search room and user
+        Room room = null;
+        room = searchRoom(roomId,hotelId);
+        User user = null;
+        user = searchUser(userId);
 
+        //break method if room isn't valid
+        if (!validateRoom(room,user)) {
+            return;
+        }
+
+        //clear User from Room
+        room.setUserReserved(null);
+
+        //set isReserved flag for false
+        room.setReserved(false);
+        System.out.println("reservation is canceled");
     }
 
-//    one of possible realization
+    // one of possible realization
     public Collection<Room> findRoom(Map<String, String> params) {
         Predicate<Room> testByParams = new Predicate<Room>() {
             @Override
@@ -177,76 +182,6 @@ public class InterfaceAPIImpl implements InterfaceAPI {
                 .collect(Collectors.toSet());
     }
 
-// not correct   
-    // receive Map with key - parameter name, value - parameter value
-    //
-    // need to receive all parameters: price, persons, hotelId
-    /*@Override
-    public Collection<Room> findRoom(Map<String, String> params) {
-        Collection<Room> result = new HashSet<>();
-
-        //check on price
-
-            if (params.containsKey("price")) {
-                result = roomDAOImpl.getRoomByPrice(Double.parseDouble(params.get("price")));
-            }
-
-        //check on persons
-        if (params.containsKey("persons")) {
-            result.addAll(roomDAOImpl.getRoomByPersons(valueOf(params.get("persons"))));
-        }
-
-        //check on hotelid
-        if (params.containsKey("hotel") || params.containsKey("hotelId")) {
-            result.addAll(roomDAOImpl.getRoomByHotelId(Long.parseLong(params.get("hotel"))));
-        }
-
-        //check on roomid
-        if (params.containsKey("room") || params.containsKey("roomId")) {
-                result.addAll(roomDAOImpl.getRoomByRoomId(Long.parseLong(params.get("room"))));
-        }
-        //else  System.out.println("Wrong parameter");
-
-        return result;
-    }*/
-
-    //searches for Room with given parameters and set isReserved in Room for false
-    @Override
-    public void cancelReservation(long roomId, long userId, long hotelId) {
-
-        //search room
-        Room roomSearch = null;
-        for (Room room : rooms) {
-            if (room.getId() == roomId && room.getHotelId() == hotelId) {
-                roomSearch = room;
-                break;
-            }
-        }
-
-        //break method if room wasn't found
-        if (roomSearch == null) {
-            System.out.println("room wasn't found");
-            return;
-        }
-
-        //break method if room isn't reserved
-        if (!roomSearch.isReserved()) {
-            return;
-        }
-
-        //break method if userReserved has different ID
-        if (roomSearch.getUserReserved().getId() != userId) {
-            System.out.println("not correct user");
-            return;
-        }
-
-        //clear User from Room
-        roomSearch.setUserReserved(null);
-
-        //set isReserved flag for false
-        roomSearch.setReserved(false);
-        System.out.println("reservation is canceled");
-    }
 
     //before start we need to register our user by MK
     public void registerUser(User user) {
